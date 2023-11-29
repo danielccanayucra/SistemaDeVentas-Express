856d45790591ef14613c24a6a42d1c16e8b0d672
DanielPOO <ccanadaniely@gmail.com>
2023-11-28 20:15:56 -0500

    cambios en el dashboar

--- routes/products.js
+++ routes/products.js
@@ -94,4 +94,119 @@ router.post('/add', function(req, res, next) {
       })
   }
   })
+/* VER FORMULARIO EDITAR*/
+router.get('/edit/(:id)', function(req, res, next) {
+
+  let id = req.params.id;
+ 
+  dbConn.query('SELECT * FROM productos WHERE id = ' + id, function(err, rows, fields) {
+      if(err) throw err
+       
+      // if user not found
+      if (rows.length <= 0) {
+          req.flash('error', 'Registro not found with id = ' + id)
+          res.redirect('/products')
+      }
+      // if book found
+      else {
+          // render to edit.ejs
+          res.render('products/edit', {
+              id: rows[0].id,
+              nombre: rows[0].nombre,
+              descripcion: rows[0].descripcion,
+              modelo: rows[0].modelo,
+              stock: rows[0].stock,
+              precio: rows[0].precio,
+              imagen: rows[0].imagen,
+              CATEGORIAS_id: rows[0].CATEGORIAS_id,
+              PROVEEDORES_id: rows[0].PROVEEDORES_id,
+          })
+      }
+  })
+})
+
+
+/* ACTUALIZAR FORMULARIO DE BASE DE DATOS*/
+router.post('/update/:id', function(req, res, next) {
+
+  let id = req.params.id;
+  let nombre = req.body.nombre;
+  let descripcion = req.body.descripcion;
+  let modelo = req.body.modelo;
+  let stock = req.body.stock;
+  let precio = req.body.precio;
+  let imagen = req.body.imagen;
+  let CATEGORIAS_id = req.body.CATEGORIAS_id;
+  let PROVEEDORES_id = req.body.PROVEEDORES_id;
+  let errors = false;
+
+  if(nombre.length === 0 || descripcion.length === 0 || modelo.length === 0 || stock.length === 0 || precio.length === 0 || imagen.length === 0 || CATEGORIAS_id.length === 0 || PROVEEDORES_id.length === 0  ) {
+      errors = true;
+      
+      // set flash message
+      req.flash('error', "Please enter name ");
+      // render to add.ejs with flash message
+      res.render('products/edit', {
+          id: req.params.id,
+          nombre: nombre,
+          descripcion: descripcion,
+          modelo: modelo,
+          stock: stock,
+          precio: precio,
+          imagen: imagen,
+          CATEGORIAS_id: CATEGORIAS_id,
+          PROVEEDORES_id: PROVEEDORES_id,
+      })
+  }
+  // if no error
+  if( !errors ) {   
+
+      var form_data = {
+          nombre: nombre,
+          descripcion: descripcion,
+          modelo: modelo,
+          stock: stock,
+          precio: precio,
+          imagen: imagen,
+          CATEGORIAS_id: CATEGORIAS_id,
+          PROVEEDORES_id: PROVEEDORES_id,
+      }
+      // update query
+      dbConn.query('UPDATE productos SET ? WHERE id = ' + id, form_data, function(err, result) {
+          //if(err) throw err
+          if (err) {
+              // set flash message
+              req.flash('error', err)
+              // render to edit.ejs
+              res.render('products/edit', {
+                  id: req.params.id,
+                  nombre: form_data.nombre,
+                  descripcion: form_data.descripcion,
+                  modelo: form_data.modelo,
+                  stock: form_data.stock,
+                  precio: form_data.precio,
+                  imagen: form_data.imagen,
+                  CATEGORIAS_id: form_data.CATEGORIAS_id,
+                  PROVEEDORES_id: form_data.PROVEEDORES_id,
+              })
+          } else {
+              req.flash('success', 'Registro successfully updated');
+              res.redirect('/products');
+          }
+      })
+  }
+})
+/* ELIMINAR REGISTRO DE BASE DE DATOS*/
+router.get('/delete/(:id)', function(req, res, next) {
+  let id = req.params.id;
+  dbConn.query('DELETE FROM productos WHERE id = ' + id, function(err, result) {
+      if (err) {
+          req.flash('error', err)
+          res.redirect('/products')
+      } else {
+          req.flash('success', 'Registro successfully deleted! ID = ' + id)
+          res.redirect('/products')
+      }
+  })
+})
 module.exports = router;
--- routes/vendedores.js
+++ routes/vendedores.js
@@ -14,5 +14,198 @@ router.get('/', function(req, res, next) {
     });
     
   });
+/* VER FORMULARIO ADD*/
+router.get('/add', function(req, res, next) {    
+  // render to add.ejs
+  res.render('vendedores/add', {
+      nombres: '',  
+      apellidos: '', 
+      dni: '', 
+      celular: '', 
+      email: '',    
+      fecnac: '', 
+      direccion: '', 
+      USUARIOS_id: '', 
+  })
+})
+/* INSERTAR EN BASE DE DATOS*/
+router.post('/add', function(req, res, next) {    
 
+  let nombres = req.body.nombres;
+  let apellidos = req.body.apellidos;
+  let dni = req.body.dni;
+  let celular = req.body.celular;
+  let email = req.body.email;
+  let fecnac = req.body.fecnac;
+  let direccion = req.body.direccion;
+  let USUARIOS_id = req.body.USUARIOS_id;
+  let errors = false;
+
+  if(nombres.length === 0 || apellidos.length === 0 || dni.length === 0 || celular.length === 0 || email.length === 0 || fecnac.length === 0  || direccion.length === 0  || USUARIOS_id.length === 0  ) {
+      errors = true;
+      req.flash('error', "Please enter name");
+      res.render('vendedores/add', {
+          nombres: nombres,
+          apellidos: apellidos,
+          dni: dni,
+          celular: celular,
+          email: email,
+          fecnac: fecnac,
+          direccion: direccion,
+          USUARIOS_id: USUARIOS_id,
+      })
+  }
+   // if no error
+   if(!errors) {
+  
+    var form_data = {
+        nombres: nombres,
+        apellidos: apellidos,
+        dni: dni,
+        celular: celular,
+        email: email,
+        fecnac: fecnac,
+        direccion: direccion,
+        USUARIOS_id: USUARIOS_id,
+    }
+    
+    // insert query
+    dbConn.query('INSERT INTO vendedores SET ?', form_data, function(err, result) {
+        //if(err) throw err
+        if (err) {
+            req.flash('error', err)
+             
+            // render to add.ejs
+            res.render('vendedores/add', {
+                nombres: form_data.nombres,
+                apellidos: form_data.apellidos,
+                dni: form_data.dni,
+                celular: form_data.celular,
+                email: form_data.email,
+                fecnac: form_data.fecnac,
+                direccion: form_data.direccion,
+                USUARIOS_id: form_data.USUARIOS_id,
+            })
+        } else {                
+            req.flash('success', 'Book successfully added');
+            res.redirect('/vendedores');
+        }
+    })
+}
+})
+/* VER FORMULARIO EDITAR*/
+router.get('/edit/(:id)', function(req, res, next) {
+
+    let id = req.params.id;
+   
+    dbConn.query('SELECT * FROM vendedores WHERE id = ' + id, function(err, rows, fields) {
+        if(err) throw err
+         
+        // if user not found
+        if (rows.length <= 0) {
+            req.flash('error', 'Registro not found with id = ' + id)
+            res.redirect('/vendedores')
+        }
+        // if book found
+        else {
+            // render to edit.ejs
+            res.render('vendedores/edit', {
+                id: rows[0].id,
+                nombres: rows[0].nombres,
+                apellidos: rows[0].apellidos,
+                dni: rows[0].dni,
+                celular: rows[0].celular,
+                email: rows[0].email,
+                fecnac: rows[0].fecnac,
+                direccion: rows[0].direccion,
+                USUARIOS_id: rows[0].USUARIOS_id,
+            })
+        }
+    })
+  })
+  
+  
+  /* ACTUALIZAR FORMULARIO DE BASE DE DATOS*/
+  router.post('/update/:id', function(req, res, next) {
+  
+    let id = req.params.id;
+    let nombres = req.body.nombres;
+    let apellidos = req.body.apellidos;
+    let dni = req.body.dni;
+    let celular = req.body.celular;
+    let email = req.body.email;
+    let fecnac = req.body.fecnac;
+    let direccion = req.body.direccion;
+    let USUARIOS_id = req.body.USUARIOS_id;
+    let errors = false;
+  
+    if(nombres.length === 0 || apellidos.length === 0 || dni.length === 0 || celular.length === 0 || email.length === 0 || fecnac.length === 0  || direccion.length === 0  || USUARIOS_id.length === 0  ) {
+        errors = true;
+        
+        // set flash message
+        req.flash('error', "Please enter name ");
+        // render to add.ejs with flash message
+        res.render('vendedores/edit', {
+            id: req.params.id,
+            nombres: nombres,
+            apellidos: apellidos,
+            dni: dni,
+            celular: celular,
+            email: email,
+            fecnac: fecnac,
+            direccion: direccion,
+            USUARIOS_id: USUARIOS_id,
+        })
+    }
+    // if no error
+    if( !errors ) {   
+  
+        var form_data = {
+            nombres: nombres,
+            apellidos: apellidos,
+            dni: dni,
+            celular: celular,
+            email: email,
+            fecnac: fecnac,
+            direccion: direccion,
+            USUARIOS_id: USUARIOS_id,
+        }
+        // update query
+        dbConn.query('UPDATE vendedores SET ? WHERE id = ' + id, form_data, function(err, result) {
+            //if(err) throw err
+            if (err) {
+                // set flash message
+                req.flash('error', err)
+                // render to edit.ejs
+                res.render('vendedores/edit', {
+                    id: req.params.id,
+                    nombres: form_data.nombres,
+                    apellidos: form_data.apellidos,
+                    dni: form_data.dni,
+                    celular: form_data.celular,
+                    email: form_data.email,
+                    fecnac: form_data.fecnac,
+                    direccion: form_data.direccion,
+                    USUARIOS_id: form_data.USUARIOS_id,
+                })
+            } else {
+                req.flash('success', 'Registro successfully updated');
+                res.redirect('/vendedores');
+            }
+        })
+    }
+  })
+/* ELIMINAR REGISTRO DE BASE DE DATOS*/
+router.get('/delete/(:id)', function(req, res, next) {
+    let id = req.params.id;
+    dbConn.query('DELETE FROM vendedores WHERE id = ' + id, function(err, result) {
+        if (err) {
+            req.flash('error', err)
+            res.redirect('/vendedores')
+        } else {
+            req.flash('success', 'Registro successfully deleted! ID = ' + id)
+            res.redirect('/vendedores')
+        }
+    })
+  })
 module.exports = router;
